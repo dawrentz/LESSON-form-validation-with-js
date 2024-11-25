1adb87ed2a5a524a00ced19075bf54910de69f94
dawrentz <156712569+dawrentz@users.noreply.github.com>
2024-11-25 12:31:25 -0600

    Set up code to connect county -> zip logic
    Decided to update the zip-code-input pattern in html on county selection
    
    USA/Mexico done, need finish Canada

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # template-repo
 
-Dedicated template repo for init new projects
+https://www.theodinproject.com/lessons/node-path-javascript-form-validation-with-javascript
+
+todo:
+-create zip code logic based on country
+-update error messages
+-create password requirement logic
+-link to confirm password logic
+-highlight invalid with css (add "touched" class to inputs?)
--- src/formValidationMod.js
+++ src/formValidationMod.js
@@ -1,11 +1,14 @@
 // formValidationMod.js
 
-//declarations
+// ======================================== Declarations ======================================== //
 const form1 = document.querySelector("#form-1");
 
+//grab all field inputs
 const allFieldInputs = document.querySelectorAll(
   "[data-special-class='form-input-field']"
 );
+const countryInput = document.querySelector("#country-input");
+const zipCodeInput = document.querySelector("#zip-code-input");
 
 // ======================================== Major Functions ======================================== //
 
@@ -33,10 +36,14 @@ function addELtoFromSubmit() {
 }
 
 function addELsToFormInputs() {
-  //Check for/update error when leave input focus. For inoffensive UI (delayed negative feedback)
+  //ELs for when the user leaves input focus. For inoffensive UI (delayed negative feedback/checks)
   allFieldInputs.forEach((fieldInput) => {
     fieldInput.addEventListener("blur", () => {
+      //Check for/update error
       showErrorMessage(fieldInput);
+
+      //add "touched class"
+      addClassToElm(fieldInput, "touched");
     });
   });
 
@@ -48,6 +55,28 @@ function addELsToFormInputs() {
         removeErrorMessage(fieldInput);
       }
     });
+  });
+
+  //on country input
+  countryInput.addEventListener("change", () => {
+    //add "touched" class to country input on country selection
+    addClassToElm(countryInput, "touched");
+
+    //update zip pattern
+    updateZipCodePattern();
+
+    //check zip code error here
+    if (zipCodeInput.classList.contains("touched")) {
+      showErrorMessage(zipCodeInput);
+    }
+  });
+
+  // on zip input (for digit counter)
+  zipCodeInput.addEventListener("input", () => {
+    //only counts if already initiated ("touched")
+    if (zipCodeInput.classList.contains("touched")) {
+      showErrorMessage(zipCodeInput);
+    }
   });
 }
 
@@ -66,7 +95,7 @@ function removeErrorMessage(inputEventElm) {
   setErrorMessage(errorElm, "");
 }
 
-//group all field inputs and list out all error cases. Like an accessable databank
+//group all field inputs and list out all error cases. Search through databank
 function getErrorMessage(inputElm) {
   let errorMessage;
 
@@ -92,12 +121,40 @@ function getErrorMessage(inputElm) {
   //need cases for other countries
   if (inputElm.id === "zip-code-input") {
     //error cases
-    if (inputElm.validity.tooShort) {
-      errorMessage = `Please enter ${inputElm.maxLength - inputElm.value.length} more digits`;
-    } else if (inputElm.value === "") {
+
+    // USA/Mexico
+    if (countryInput.value === "US" || countryInput.value === "MX") {
+      //zip is too short or contains non-numbers
+
+      if (inputElm.validity.patternMismatch) {
+        errorMessage = "Please enter five digit zip code";
+      }
+
+      if (inputElm.validity.tooShort) {
+        //test
+        console.log("short/bad");
+        errorMessage = `Please enter ${inputElm.maxLength - inputElm.value.length} more digits`;
+      }
+    }
+
+    // Canada
+    if (countryInput.value === "CA") {
+      if (inputElm.validity.patternMismatch) {
+        errorMessage =
+          'Please follow pattern: "ANA NAN" (A = alpha, N = number)';
+      }
+    }
+    //run checks for each
+    //  (a = alpha, n = number), <br> no D, F, I, O, Q, or U anywhere and no W and Z for first letter';
+
+    //no input
+    if (inputElm.value === "") {
       errorMessage = "Please enter your zip code";
-    } else if (inputElm.validity.patternMismatch) {
-      errorMessage = "Please enter 5 digit zip code";
+    }
+
+    //no country selected
+    if (countryInput.value === "") {
+      errorMessage = "Please select country of residence first";
     }
   }
 
@@ -120,6 +177,31 @@ function getErrorMessage(inputElm) {
   return errorMessage;
 }
 
+function updateZipCodePattern() {
+  //possible values: "", CA, MX, US, "other"
+  const countrySelection = countryInput.value;
+
+  if (countrySelection === "" || countrySelection === "other") {
+    zipCodeInput.setAttribute("pattern", "");
+    zipCodeInput.setAttribute("minLength", "");
+    zipCodeInput.setAttribute("maxLength", "");
+  }
+  if (countrySelection === "US" || countrySelection === "MX") {
+    zipCodeInput.setAttribute("pattern", "^\\d{5}$");
+    zipCodeInput.setAttribute("minLength", "5");
+    zipCodeInput.setAttribute("maxLength", "5");
+  }
+  if (countrySelection === "CA") {
+    //pattern is "ANA NAN" (a = alpha, n = number), no D, F, I, O, Q, or U anywhere and no W and Z for first letter
+    zipCodeInput.setAttribute(
+      "pattern",
+      "^[ABCEGHJKLMNPRSTVXY]\\d[ABCEGHJKLMNPRSTVWXYZ] \\d[ABCEGHJKLMNPRSTVWXYZ]\\d$"
+    );
+    zipCodeInput.setAttribute("minLength", "7");
+    zipCodeInput.setAttribute("maxLength", "7");
+  }
+}
+
 // ======================================== Generic Functions ======================================== //
 
 function getErrorElm(inputElm) {
@@ -132,4 +214,12 @@ function getErrorElm(inputElm) {
 
 function setErrorMessage(elm, message) {
   elm.textContent = message;
+}
+
+function isOnlyNumbers(str) {
+  return /^[0-9]+$/.test(str);
+}
+
+function addClassToElm(elm, className) {
+  elm.classList.add(className);
 }
